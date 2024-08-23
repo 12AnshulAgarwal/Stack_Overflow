b4c81d544cbc160f859e3b3d7e802f71313dec3a
12AnshulAgarwal <1210anshulagarwal@gmail.com>
2024-08-23 18:01:30 +0530

    navbar part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState} from 'react';
 import Navbar from './Component/Navbar/Navbar';
+import { useEffect } from 'react';
 function App() {
+    const[slidein,setslidein]=useState(true)
+    useEffect(()=>{
+      if(window.innerWidth<=768){
+        setslidein(false)
+      }
+    },[])
+    const handlesslidein=()=>{
+      if(window.innerWidth<=768){
+        setslidein((state)=>!state)
+      }
+    };
   return (
     <div className="App">
       <Navbar/>
-      <h1></h1>
+      <h1>Hello</h1>
     </div>
     
   );

a40a5f8e2880b796a2dd7063009127116bb9f0d1
12AnshulAgarwal <1210anshulagarwal@gmail.com>
2024-08-24 10:07:26 +0530

    Commits

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect,useState} from 'react';
-import Navbar from './Component/Navbar/Navbar';
-import { useEffect } from 'react';
 function App() {
     const[slidein,setslidein]=useState(true)
     useEffect(()=>{
@@ -15,12 +13,11 @@ function App() {
         setslidein((state)=>!state)
       }
     };
+
   return (
     <div className="App">
-      <Navbar/>
-      <h1>Hello</h1>
-    </div>
-    
+      <Navbar handlesslidein={handlesslidein}/>
+    </div> 
   );
 }
 
--- client/src/Component/Navbar/Navbar.jsx
+++ client/src/Component/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
-
+import { Link,useNavigate } from 'react-router-dom'
+import { useSelector,useDispatch } from 'react-redux'
+import bars from '../../assets/bars-solid.svg'
+import logo from '../../assets/logo.png'
+import search from '../../assets/search-solid.svg'
 function Navbar() {
+  var User=null;
   return (
-    <div>
-      
-    </div>
+      <><nav className="main-nav">
+      <div className="Navbar">
+        <button className="slide-in-icon">
+          <img src={bars} alt="bars" width='15' />
+        </button>
+        <div className="navbar1">
+          <Link to='/' className="nav-item nav-logo">
+            <img src={logo} alt='logo'></img></Link>
+          <Link to='/' className="nav-item nav-btn res-nav">
+            About</Link>
+          <Link to='/' className="nav-item nav-btn res-nav">
+            Products</Link>
+          <Link to='/' className="nav-item nav-btn res-nav">
+            For Teams</Link>
+          <Link to='/' className="nav-item nav-btn res-nav">
+            For Teams</Link>
+            <form><input type="text" placeholder='Search...' /></form>
+        </div>
+      </div>
+
+
+    </nav><div>
+
+      </div></>
   )
 }
 
